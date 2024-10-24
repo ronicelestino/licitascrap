a23c7f577d460df8a46fe6c40f4fdbcb6a6f506c
Roni Celestino <ronicelestino@gmail.com>
2024-10-24 13:33:49 -0300

    Configuração inicial do projeto java com Spring

---
+++ src/test/java/com/roni/licitascrap/LicitaScrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roni.licitascrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicitaScrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
